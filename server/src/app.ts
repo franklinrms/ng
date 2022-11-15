897b9e823bc39c660e401eb53986f5a3628dd6a2
Franklin Ramos <franklinramos@outlook.com>
2022-11-15 12:54:08 -0300

    chore: initial settings

---
+++ server/src/app.ts
@@ -0,0 +1,22 @@
+import express from 'express';
+import cors from 'cors';
+import 'express-async-errors';
+
+export default class App {
+  public app: express.Express;
+
+  constructor() {
+    this.app = express();
+    this.config();
+    this.app.get('/', (_req, res) => res.json({ ok: true }));
+  }
+
+  private config():void {
+    this.app.use(cors());
+    this.app.use(express.json());
+  }
+
+  public start(PORT: string | number):void {
+    this.app.listen(PORT, () => console.log(`Running on port ${PORT}`));
+  }
+}
